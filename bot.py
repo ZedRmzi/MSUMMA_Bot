1ffe818feab2bd9bb6b7068dda6504d9757ce864
ZedRmzi <zaid-ramzi@hotmail.com>
2022-01-29 02:26:57 -0500

    Hello command to test send functionality

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@
 Code for the discord bot goes here
 """
 from TOKEN import TOKEN
-import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='$')
@@ -12,6 +11,10 @@ bot = commands.Bot(command_prefix='$')
 @bot.event
 async def on_ready():
     print("Ready")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
 
 
 bot.run(TOKEN)
